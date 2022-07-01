176a50675171c0648efa33f251a7f69ab4dfe349
onanefe_okotie <onanefeokotie75@gmail.com>
2022-06-30 19:20:28 -0700

    Nw

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
- * Description: Use C programming to print text, a new line and a return of 1
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n
-	" , 59);
-        return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
