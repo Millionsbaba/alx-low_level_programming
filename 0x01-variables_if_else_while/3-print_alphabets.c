a5d1340bdcf234839a3ab2c3e5c257d2e9eed013
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-01 19:13:18 -0700

    New code for printing alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: 'A new code to print upper and lower case alphabet'
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+	        letter++;
+	}
+
+	letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+	        letter++;
+	}
+
+		putchar('\n');
+		return (0);
+}
