344077df2554c8bda01d22eb67504875962be1ed
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-04 09:24:11 -0700

    A source code to print special numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print various combinations of base 10 numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c;
+	int d;
+
+	for (d = 0; d < 10; d++)
+	{
+		for (c = 0; c < 10; c++)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(46);
+					putchar(32);
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
