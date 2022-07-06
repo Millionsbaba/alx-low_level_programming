e084edc0e1db656bb3dbb049c33b89640a7228b1
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-06 16:15:39 -0700

    New source code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower: A new code to print lower case c
+ * @c: A character argument
+ *
+ * Return: 1 or 0 based on the condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
