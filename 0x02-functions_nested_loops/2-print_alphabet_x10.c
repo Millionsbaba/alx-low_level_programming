ab7dd8b94d354ca1cf8457de9453500c03e69ce5
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-06 15:16:21 -0700

    New source code to print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A source code to print alphabets 10x
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	int i;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar('\n')
+}
