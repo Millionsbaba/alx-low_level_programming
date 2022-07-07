a7082eec1a774b6ef3dacaca7219751ec8d38a71
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-06 18:54:42 -0700

    Absolute valuee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - A new code to print absolute value
+ * @n: Description for n
+ *
+ * Return: Based on condition
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+}
