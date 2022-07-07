8c970ac533a08cb96d9034dd2ad1a5da2e1fed22
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-06 17:44:29 -0700

    New code to print all laphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - My code is to procuce lowercase and uppercase alphabets
+ * @c:This tells us about c
+ *
+ * Return: 0 or 1 depending on the condition
+ */
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
