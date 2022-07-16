706655f1f35b32f353ea84ef2cb84f8f02c53e36
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-16 03:37:25 -0700

    New source code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,28 +10,16 @@
  */
 int main(void)
 {
-	int i, sum, n;
-	int pass[100];
-
-	sum = 0;
+	int random, iteration, max;
 
 	srand(time(NULL));
-
-	for (i = 0; i < 100; i++)
+	for (iteration = 0, max = 2772; max > 122; iteration++)
 	{
-		pass[i] = rand() % 78;
-		sum += (pass[i] + '0');
-		putchar(pass[i] + '0');
-
-		if ((2772 - sum) - '0' < 78)
-		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
-			break;
-		}
+		random = (rand() % 125) + 1;
+		printf("%c", random);
+		max -= random;
 	}
+	printf("%c", max);
 
 	return (0);
-
 }
