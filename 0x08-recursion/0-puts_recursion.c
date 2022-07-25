bd4a5247aae0f53f52f59adaa9befe856c4689ea
onanefe_okotie <onanefeokotie75@gmail.com>
2022-07-25 03:20:21 -0700

    Recursive functions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string by implementing recursion
+ * @s: Source memory address
+ *
+ * Return: Nothing.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		_putchar(*s);
+	}
+
+	else
+	{
+		_putchar(10);
+		return;
+	}
+
+	s++;
+	_puts_recursion(s);
+}
